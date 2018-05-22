053afc9a506415bf1e8b7b6c107de62ee1f1c1a3
Alberto Paz <aj.paz167@gmail.com>
2018-05-22 13:01:20 +0200

    Add files via upload

--- models/base_a.py
+++ models/base_a.py
@@ -9,23 +9,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from layers.squeeze_embedding import SqueezeEmbedding
-# TO DO : FIX ATTENTION LAYER, FREEZE EMBEDDINGS
+
 class BaseA(nn.Module):
-    '''
-    Content Attention Module
-    
-    Attention weights of each memory slice m_i is calculated with a FwNN2
-    c_i = W_1 tanh(W2_m_i + W_3v_a + b_1)
-    '''
     def __init__(self, embedding_matrix, opt):
         super(BaseA, self).__init__()
         self.opt = opt
         self.embed = nn.Embedding.from_pretrained(torch.tensor(embedding_matrix, dtype=torch.float))
         self.squeeze_embedding = SqueezeEmbedding(batch_first=True)
-        self.attention = Attention(opt.embed_dim, score_function='mlp')
-        self.x_linear = nn.Linear(opt.embed_dim, opt.embed_dim)
-        self.mlp = nn.Linear(opt.embed_dim, opt.embed_dim)                                                # W4
-        self.dense = nn.Linear(opt.embed_dim, opt.polarities_dim)                                         # W5
+        self.attention = Attention(opt.embed_dim, score_function='a')
+        self.m_linear = nn.Linear(opt.embed_dim, opt.embed_dim)                                 # W2
+        self.x_linear = nn.Linear(opt.embed_dim, opt.embed_dim)                                 # W3
+        self.mlp = nn.Linear(opt.embed_dim, opt.embed_dim)                                      # W4
+        self.dense = nn.Linear(opt.embed_dim, opt.polarities_dim)                               # W5
 
     def forward(self, inputs):
         
@@ -35,18 +30,21 @@ class BaseA(nn.Module):
         aspect_len = torch.sum(aspect_indices != 0, dim=-1)
         nonzeros_aspect = torch.tensor(aspect_len, dtype=torch.float).to(self.opt.device)
         
+        # aspect representation
+        aspect = self.embed(aspect_indices)
+        aspect = torch.sum(aspect, dim=1)
+        aspect = torch.div(aspect, nonzeros_aspect.view(nonzeros_aspect.size(0), 1))
+        x = aspect.unsqueeze(dim=1)
+        
         # memory module
         memory = self.embed(text_raw_indices)
         memory = self.squeeze_embedding(memory, memory_len)
         
         # content attention module
-        aspect = self.embed(aspect_indices)
-        aspect = torch.sum(aspect, dim=1)
-        aspect = torch.div(aspect, nonzeros_aspect.view(nonzeros_aspect.size(0), 1))
-        x = aspect.unsqueeze(dim=1)
         for _ in range(self.opt.hops): 
             x = self.x_linear(x)
-            v_ns = self.attention(memory, x)              # aspect-specific sentence representation (embedding)
+            memory = self.m_linear(memory)
+            v_ns = self.attention(memory, x)              # aspect-specific sentence representation
         
         # classifier
         v_ns = v_ns.view(v_ns.size(0), -1)
